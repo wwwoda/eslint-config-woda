f6a935d2c348ed35f93110cff6b6d3c3575ba486
David Mondok <3883758+davidmondok@users.noreply.github.com>
2021-09-16 13:11:59 +0200

    readd peerdeps

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-module.exports = {
-  extends: ['./base.js'],
-};
+const eslintrc = require('./.eslintrc');
+
+module.exports = eslintrc;
